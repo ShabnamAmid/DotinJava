ff9bfcb69d248745f7824e976e8489a2b13211ba
Your Name <you@example.com>
2024-05-27 13:00:18 +0330

    Lecture2

--- Section2.java
+++ Section2.java
@@ -1,2 +1,14 @@
-package scr;public class Section2 {
+package scr;
+
+public class Section2 {
+    public static void main(String[] args) {
+        String name = "Shabnam";
+        String family = "Amid";
+        int age = 41;
+        String genter = "Female";
+        System.out.println("Name: " + name + "\n" +
+        "Family: " + family + "\n" +
+        "Age: "+ age + "\n" +
+        "Gender:  "+ genter);
+    }
 }
