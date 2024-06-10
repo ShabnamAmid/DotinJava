59630c250d3b1aa4079a0a90ea5a9da6f3c6bbe4
Your Name <you@example.com>
2024-06-10 17:43:36 +0330

    Lecture3

--- App.java
+++ App.java
@@ -1,8 +1,22 @@
 package scr;
 
+import java.net.SocketOption;
+import java.sql.SQLOutput;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello Dotin!!");
+        long age = 80L;
+        long max = Long.MAX_VALUE;
+        float f2 = 1241.51f;
+        double d1 = 521512.251452;
+        boolean b1 = false;
+
+        System.out.println(age);
+        System.out.println(f2);
+        System.out.println(max);
+        System.out.println(d1);
+        System.out.println(b1);
     }
 }
 
